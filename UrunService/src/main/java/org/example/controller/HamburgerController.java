7f347c177b2aaee9e9f84526d1efaae794b80372
celestialalpaca <denizgumus1996@hotmail.com>
2024-06-13 17:36:39 +0300

    31

--- UrunService/src/main/java/org/example/controller/HamburgerController.java
+++ UrunService/src/main/java/org/example/controller/HamburgerController.java
@@ -9,10 +9,7 @@ import org.example.service.HamburgerService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,14 +25,14 @@ public class HamburgerController
 
     @PostMapping(SAVE)
     @PreAuthorize("hasAnyAuthority('ADMIN')")
-    public ResponseEntity<String> save(HamburgerSaveRequest dto)
+    public ResponseEntity<String> save(@RequestBody HamburgerSaveRequest dto)
     {
         return ResponseEntity.ok(hamburgerService.save(dto));
     }
 
     @GetMapping(FINDBYID)
     @PreAuthorize("hasAnyAuthority('USER')")
-    public ResponseEntity<HamburgerModel> findById(Long hamburgerId)
+    public ResponseEntity<HamburgerModel> findById(@RequestParam Long hamburgerId)
     {
         return ResponseEntity.ok(hamburgerService.find(hamburgerId));
     }
